d29d8539f127f95a1b93f7d2c2fed10989323367
Alex Davies <traverse.da@gmail.com>
2019-08-19 19:35:24 -0300

    Regenerated rawCimgui.pxd

--- generate_raw_bindings.py
+++ generate_raw_bindings.py
@@ -7,7 +7,20 @@ for name in sources:
     data = json.loads(open(sourceRoot+name,"r").read())
     sourceData[name.replace(".json","")]=data
 
+
 from jinja2 import Template
 t = Template(open("template_rawCimgui.pxd","r").read())
 
-print(t.render(**sourceData))
+output = (t.render(**sourceData))
+
+#Sometimes we get a name collision between a python
+#name and a C name, this simply replaces all instances
+#of key with value to get around the collisions.
+reserved_names = {
+#    "char*(": "_char*(",
+}
+
+for key, value in reserved_names.items():
+    output = output.replace(key, value)
+
+print(output)
